e5a7c2ecf05d8923a537a8b9c633e2fab3ee78ef
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2022-01-22 17:10:38 +0000

    just some stuff

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  
   validates :movie, uniqueness: { scope: :list, message: "already added to this list. Try another." }
   validates :comment, length: { minimum: 6 }
 end
